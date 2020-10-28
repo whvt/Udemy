cac8824d697638ce6feee73d249647792b741b84
Nikita <xm987654321@gmail.com>
2020-10-29 00:18:27 +0300

    complete task2

---
+++ task2/script.js
@@ -0,0 +1,33 @@
+"use strict";
+let numberOfFilms = +prompt("Cколько фильмов Вы уже посмотрели?", "");
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+//1
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
+
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("Error");
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Мало");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Error");
+}
